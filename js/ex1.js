9ac09c828b8056453ee69a4e5513a83c9c38d4f6
mahmuda-farha <mahmudafarhabdm@gmail.com>
2024-11-17 15:17:48 +0600

    first commit

---
+++ js/ex1.js
@@ -0,0 +1,88 @@
+
+document.getElementById("student_message").innerHTML = "Hi, my name is Mahmuda Farha, I'm 22 years old and I'm learning Javascript";
+document.getElementById("result").innerHTML ="14.429"
+
+
+var phone1 = "988866552";
+var phone2 = "99087612366";
+var phone3 = "876543123";
+
+if (phone1. length <= 9){
+    console.log(phone1 + "is vaild")
+}
+else (phone1. length <= 9) 
+{
+    console.log(phone1 + "is not vaild")
+}
+if (phone2. length <= 9){
+    console.log(phone2 + "is vaild")
+}
+else (phone2. length <= 9) 
+{
+    console.log(phone2 + "is not vaild")
+}
+if (phone3. length <= 9){
+    console.log(phone3 + "is vaild")
+}
+else (phone3. length <= 9) 
+{
+    console.log(phone3 + "is not vaild")
+}
+
+var power = Math.pow(32,6) 
+console.log(power)
+
+
+var quantity = "25";
+    var number = 6;
+    var pressure;
+    var temperature = null;
+
+    console.log(quantity += quantity); 
+    console.log( (7+5) / number + 2 ); 
+    console.log(pressure); 
+    console.log(temperature); 
+    console.log(typeof pressure); 
+    console.log(typeof temperature);
+    
+
+
+    var getAddress = document.getElementById("url_1").innerText;
+var modifiedAddress = getAddress + " " +"https:// here"
+
+document.getElementById("url_1").innerText = modifiedAddress
+
+
+function divisionNumbers(c) {
+    var result = (9 * c / 5) + 32
+    return result;
+}
+var divisionResult = divisionNumbers(38);
+console.log(divisionResult);
+document.getElementById("fahr_1").innerHTML = divisionResult;
+
+function divisionNumbers(c) {
+    var result = (9 * c / 5) + 32
+    return result;
+}
+var divisionResult = divisionNumbers(28);
+console.log(divisionResult);
+document.getElementById("fahr_2").innerHTML = divisionResult;
+
+
+function divisionNumbers(c) {
+    var result = (9 * c / 5) + 32
+    return result;
+}
+var divisionResult = divisionNumbers(-1);
+console.log(divisionResult);
+document.getElementById("fahr_3").innerHTML = divisionResult;
+
+
+
+var classification = [ "John Hill" , "Mary Jane", "Gary Vee", "Paricia Mills", "Helen Hall", "Paul Green" ]; 
+
+
+
+
+
